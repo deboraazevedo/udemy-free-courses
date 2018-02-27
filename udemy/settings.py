a51866533a4ba75259dc6094e6391a51b85b47bc
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2018-02-27 13:31:24 -0300

    Add heroku

--- udemy/settings.py
+++ udemy/settings.py
@@ -18,13 +18,14 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*5nq$hogvjy^-m6c8fg3k#lcn4my^55!_onxa*hfgehlqgpsb@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,6 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 TOKEN = config('TOKEN')
